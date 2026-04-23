fd7a8d9fb046e31abe09e554798225887a6efac3
PompChomp <vidhan01@easv365.dk>
2026-04-23 10:38:08 +0200

    titles
    supposedly it works....... or it may not
    I'll never know ¯\_(ツ)_/¯

--- portfolio/src/router/index.js
+++ portfolio/src/router/index.js
@@ -8,18 +8,34 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: "Home - My Portfolio",
+      },
     },
     {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue'),
+      meta: {
+        title: "About - My Portfolio",
+      },
     },
     {
       path: '/work',
       name: 'work',
       component: () => import('../views/WorkView.vue'),
+      meta: {
+        title: "Work - My Portfolio",
+      },
     }
   ],
 })
+
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`
+  next()
+})
+
 
 export default router
